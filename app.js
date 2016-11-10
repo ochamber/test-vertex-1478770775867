9d508ee1b0248a9e300966d8a412ad5f0eebe644
OLIVIER CHAMBERAUD <olivier.chamberaud@fr.ibm.com>
2016-11-10 04:05:11 -0600

    appjs

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+//app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
